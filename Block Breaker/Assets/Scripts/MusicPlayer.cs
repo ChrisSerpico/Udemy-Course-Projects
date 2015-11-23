f096ab10c47f54128e6c7985719d1aedad34a15c
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2015-11-23 16:59:05 -0600

    Lecture 64

--- Block Breaker/Assets/Scripts/MusicPlayer.cs
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -4,10 +4,21 @@ using System.Collections;
 public class MusicPlayer : MonoBehaviour
 {
 
+    static MusicPlayer instance = null;
+    
     // Use this for initialization
     void Start()
     {
-        GameObject.DontDestroyOnLoad(this.gameObject);
+        if (instance != null)
+        {
+            Debug.Log("Duplicate Music Player self-destructing");
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            GameObject.DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
     }
 
     // Update is called once per frame
