d93bd99504538c95e8ffbdd6f44f93c21e9c3145
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2015-12-08 00:38:16 -0600

    Lecture 74

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public Paddle paddle;
+
+    private Vector3 paddleToBallVector;
+
+    private bool hasStarted = false;
+    
+    // Use this for initialization
+	void Start () {
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasStarted)
+        {
+            // lock the ball relative to the paddle
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+
+            // wait for a mouse press to launch 
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+                hasStarted = true;
+            }
+        }
+	}
+}
