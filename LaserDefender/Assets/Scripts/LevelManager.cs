148ae7b34cabb19c7c53c18893ba1bd7fe0868ff
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2016-02-24 21:19:29 -0600

    Lecture 101
    Set up menu system

---
+++ LaserDefender/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour 
+{
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        Application.LoadLevel(name);
+    }
+
+    public void LoadLevel(int index)
+    {
+        Debug.Log("Level load requested for level with index " + index);
+        Application.LoadLevel(index);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit game request received");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        LoadLevel(Application.loadedLevel + 1);
+    }
+}
---
+++ LaserDefender/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    static MusicPlayer instance = null;
+    
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("Duplicate Music Player self-destructing");
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            GameObject.DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
