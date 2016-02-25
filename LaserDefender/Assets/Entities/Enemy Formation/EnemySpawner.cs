9e8aa926b749af1631bc1b9b124db7a2d8f1d5fd
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2016-02-25 02:00:29 -0600

    Lecture 105

---
+++ LaserDefender/Assets/Entities/Enemy Formation/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    
+    // Use this for initialization
+	void Start () 
+    {
+        foreach(Transform child in transform)
+        {
+            GameObject enemy = (GameObject)Instantiate(enemyPrefab, child.transform.position, Quaternion.identity);
+            enemy.transform.parent = child;
+        }
+	}
+}
