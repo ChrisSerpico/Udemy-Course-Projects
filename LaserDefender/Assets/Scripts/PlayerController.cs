70f3fd4c0ebcccd44b27c46c65ce9ac3979071d2
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2016-02-24 23:20:12 -0600

    Lecture 103

--- LaserDefender/Assets/Scripts/PlayerController.cs
+++ LaserDefender/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,39 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     public float speed = 0.5f;
+    public float padding = 0.5f; // padding between player and edge of screen 
+
+    float xmin;
+    float xmax;
+
 	
-	// Update is called once per frame
+	void Start()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftmost.x + padding;
+        xmax = rightmost.x - padding;
+    }
+    
+    // Update is called once per frame
 	void Update () 
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+            //transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+
+            transform.position += Vector3.left * speed * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            //transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+
+            transform.position += Vector3.right * speed * Time.deltaTime;
         }
+
+        // restrict player to gamespace
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
 	}
 }
