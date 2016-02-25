12c2ce7a70d7495358b4a95ecfcf16225b8378c5
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2016-02-24 21:34:35 -0600

    Lecture 102
    Added a controllable player ship

---
+++ LaserDefender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 0.5f;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+	}
+}
