c56e539c7b61d1607470fcdd3e7daba035a68d92
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2016-02-26 22:20:12 -0600

    Lecture 106

--- LaserDefender/Assets/Entities/Enemy Formation/EnemySpawner.cs
+++ LaserDefender/Assets/Entities/Enemy Formation/EnemySpawner.cs
@@ -4,6 +4,14 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour {
 
     public GameObject enemyPrefab;
+
+    public float width = 10f;
+    public float height = 5f;
+
+    public float speed = 5f;
+    private bool movingRight = true;
+    private float xmin, xmax;
+    private float padding = -0.25f;
     
     // Use this for initialization
 	void Start () 
@@ -13,5 +21,32 @@ public class EnemySpawner : MonoBehaviour {
             GameObject enemy = (GameObject)Instantiate(enemyPrefab, child.transform.position, Quaternion.identity);
             enemy.transform.parent = child;
         }
+
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftmost.x + padding;
+        xmax = rightmost.x - padding;
 	}
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+    }
+
+    void Update()
+    {
+        if (movingRight)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            if (transform.position.x + width / 2f > xmax)
+                movingRight = false;
+        }
+        else
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+            if (transform.position.x - width / 2f < xmin)
+                movingRight = true; 
+        }
+    }
 }
