45cc8a22642792697b9d5eb80acdb4122c8dd0cf
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2016-04-25 22:37:13 -0500

    Lecture 120

--- LaserDefender/Assets/Scripts/MusicPlayer.cs
+++ LaserDefender/Assets/Scripts/MusicPlayer.cs
@@ -5,11 +5,18 @@ public class MusicPlayer : MonoBehaviour
 {
 
     static MusicPlayer instance = null;
+
+    public AudioClip startClip;
+    public AudioClip gameClip;
+    public AudioClip endClip;
+
+    private AudioSource music;
+    
     
     // Use this for initialization
     void Awake()
     {
-        if (instance != null)
+        if (instance != null && instance != this)
         {
             Debug.Log("Duplicate Music Player self-destructing");
             Destroy(this.gameObject);
@@ -18,12 +25,31 @@ public class MusicPlayer : MonoBehaviour
         {
             GameObject.DontDestroyOnLoad(this.gameObject);
             instance = this;
+            music = GetComponent<AudioSource>();
+            music.clip = startClip;
+            music.loop = true;
+            music.Play();
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnLevelWasLoaded(int level)
     {
+        Debug.Log("Music Player: Loaded level " + level);
+        music.Stop();
 
+        if (level == 0)
+        {
+            music.clip = startClip;
+        }
+        if (level == 1)
+        {
+            music.clip = gameClip;
+        }
+        if (level == 2)
+        {
+            music.clip = endClip;
+        }
+
+        music.Play();
     }
 }
