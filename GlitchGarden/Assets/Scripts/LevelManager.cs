c5a1949544c38ff215d9c68d0a708f85594cde62
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2016-04-28 21:19:59 -0500

    Lecture 127

---
+++ GlitchGarden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour 
+{
+
+    public float autoLoadTime;
+    
+    void Start()
+    {
+        Invoke("LoadNextLevel", autoLoadTime);
+    }
+    
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadLevel(int index)
+    {
+        Debug.Log("Level load requested for level with index " + index);
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit game request received");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        LoadLevel(Application.loadedLevel + 1);
+    }
+}
